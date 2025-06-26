690edd0956c730c172b0b52f5366344bf6c962f4
Vasanth <vasanthraja1011@gmail.com>
2025-06-26 15:42:00 +0530

    Add files via upload

---
+++ HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Menu from '../components/Menu';
+import ReservationForm from '../components/ReservationForm';
+import '../styles/HomePage.css';
+
+const HomePage = () => (
+  <main>
+    <Menu />
+    <ReservationForm />
+  </main>
+);
+
+export default HomePage;
